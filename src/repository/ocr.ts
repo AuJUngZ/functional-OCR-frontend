bcb16bc552c11242c50d825ad5d23ec304d684b4
AuJung <nuttapong6280@gmail.com>
2025-03-09 13:01:39 +0700

    refactor: remove console log from postOcr function

--- src/repository/ocr.ts
+++ src/repository/ocr.ts
@@ -5,7 +5,5 @@ import { safeFetch } from '../utils/httpRequest'
 export const postOcr = async (
     options: RequestInit
 ): Promise<Result<OCRResponse>> => {
-    console.log('options', options)
-
     return await safeFetch<OCRResponse>('/api/ocr', options)
 }
